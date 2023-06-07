c93a1ca92141551068b2a71bad9f4f1a6925e500
JasmineOjeda <jasmineojeda01@gmail.com>
2023-06-07 13:13:34 -0700

    Update Resume

---
+++ header.js
@@ -0,0 +1,9 @@
+class Header extends HTMLElement {
+    constructor() {
+      // Always call super first in constructor
+      super();
+  
+      // write element functionality in here
+      
+    }
+  }
